282822e060266dba35759026cb5a0310549b3539
sophiegsmith <sophgsuds@gmail.com>
2021-10-12 15:25:11 -0700

    added the template and saved pdf to this file

---
+++ CS1A/Lab9/lab9.cpp
@@ -0,0 +1,24 @@
+/***********************************************************************************
+* AUTHOR : Sophie Smith
+* STUDENT ID : 123456
+* LAB #7 : Eclipse Tutorial Lab
+* CLASS : CS1A
+* SECTION : MW: 8am
+* DUE DATE : 12/20/26
+***********************************************************************************/
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+/****************************************************************************
+* INTRO TO PROGRAMMING
+*---------------------------------------------------------------------------
+* This program will read in two values and output the sum.
+*---------------------------------------------------------------------------
+* INPUT:
+* firstNum – First value to be summed.
+* secondNum - Second value to be summed.
+*
+* OUTPUT:
+* sum – The sum of the two values input.
+***************************************************************************/
