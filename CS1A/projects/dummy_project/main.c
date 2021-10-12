cf04f8577d729d55030e09fe9a4e157b808bfc9b
sophiegsmith <sophgsuds@gmail.com>
2021-10-12 15:18:58 -0700

    This is my partner lab9

--- CS1A/projects/dummy_project/main.c
+++ CS1A/projects/dummy_project/main.c
@@ -1,7 +1,9 @@
-```c
+
 void main (int argc, char* argv[])
 {
-    printf("Hello World!);
-    return;
+    /*need to add comments to explain your algorithm*/
+    //this is my hello world program
+    printf("Hello World!");
+    /*no return is needed since the fucntion know how to return by itself*/
 }
-```
+//Good Job!!! see you next code review, bye for now
