8496792bd575be866cf96953fca444fbf5384582
sophiegsmith <sophgsuds@gmail.com>
2021-10-24 09:44:21 -0700

    lab 11 cpmplete

--- CS1A/Lab11/lab11.cpp
+++ CS1A/Lab11/lab11.cpp
@@ -34,19 +34,6 @@ using namespace std;
 
 int main()
 {
-// Declare your constants here include a table describing them
-const int PROMPT_COL = 32;
-const int NAME_SIZE = 25;
-
-
-// Declare your variables here -- include your data table
-double amtLeftover;             //INPUT & CALCULATE
-double spent;                   // INPUT & CALCULATE
-char name[NAME_SIZE];
-/**********************************************************************
-* OUTPUT - class heading (from eclipse lab here)
-**********************************************************************/
-
 /***********************************************************************
     * CONSTANTS
     * ---------------------------------------------------------------------
@@ -64,35 +51,63 @@ char name[NAME_SIZE];
     const char SECTION[]     = "MW: 8am - 10:20";
     const int  LAB_NUM       = 11;
     const char LAB_NAME[]    = "Basic Input & Output";
+    
+//constants for program 
+const float WEEKLY_POCKET_MONEY = 20.00;
+const int NAME_SIZE = 25;
+
+// Declare your variables here -- include your data table
+double amtLeftover;             // INPUT & CALCULATE
+double spent;                   // INPUT & CALCULATE
+char name[NAME_SIZE];           // INPUT 
+float currentMoney;
+float pocketMoney; 
 
 /**********************************************************************
-* INPUT: (description of your input)
+* OUTPUT - class heading (from eclipse lab here)
 **********************************************************************/
-
-cout << left;
-cout << PROMPT_COL<< "What is your name? ";
-cin.getline(name,NAME_SIZE);
-cout << PROMPT_COL<< "How much is left from last week? ";
-cin >> amtLeftover;
-cin.ignore(1000, '/n');
-cout << PROMPT_COL<< "How much have you spent? ";
-cin >> spent;
-cin.ignore (1000, '/n');
-cout << right; 
+    cout << left;
+    cout << "**************************************************\n";
+    cout << "* PROGRAMMED BY : " << PROGRAMMER1 << endl;
+    cout << "* PROGRAMMED BY : " << PROGRAMMER2 << endl;
+    cout << "* " << setw(14) << "CLASS" << ": " << CLASS << endl;
+    cout << "* " << setw(14) << "SECTION" << ": " << SECTION << endl;
+    cout << "* LAB #" << setw(9) << LAB_NUM << ": " << LAB_NAME << endl;
+    cout << "**************************************************\n\n";
+    cout << right;
 
 /**********************************************************************
-* PROCESSING: (description of your processing)
+* INPUT: (prompts the user their name, amount left, and amount spent)
 **********************************************************************/
-
+    cout << left;
+    cout << setw(32) << "What's your name? " <<endl; 
+    cin.getline(name, NAME_SIZE);
+    cout << setw(32) << "How much is left from last week? " << endl;
+    cin >> amtLeftover;
+    cout << setw(32) << "How much have you spent? " << endl;
+    cin >> spent;
+    cout << endl << right << endl;
 
 
 /**********************************************************************
-* OUTPUT: (description of your output)
+* PROCESSING: (calculates the currentMoney, and the pocketMoney)
 **********************************************************************/
+    
+    currentMoney = spent - amtLeftover;
+    pocketMoney = WEEKLY_POCKET_MONEY - currentMoney;
 
-
-
-
+/**********************************************************************
+* OUTPUT: (outputs the name, amtLeftover, spent, and pocketMoney)
+**********************************************************************/
+    cout << left; 
+    cout << fixed;
+    cout << setprecision(2); 
+    cout << setw(32)<< "What's your name? " << name << endl; 
+    cout << setw(32)<< "How much is left from last week " << amtLeftover << endl;
+    cout << setw(32)<< "How much have you spent? " << spent << endl;
+    cout << endl << endl; 
+    cout << "Hello " << name << "!" << endl;
+    cout << "You now have $" << pocketMoney << " left. " << endl;
 
 return 0;
 }
